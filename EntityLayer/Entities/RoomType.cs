daa32bc2baf795f4f5647ae45b9b5a28cf2fb68c
murat ciplak <muratapk@hotmail.com>
2023-12-11 15:37:03 +0300

    Add project files.

---
+++ EntityLayer/Entities/RoomType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class RoomType
+    {
+        [Key]
+        public int RoomTypeId { get; set; }
+        public string RoomName { get; set; }
+        public double RoomPrice {  get; set; }  
+        public virtual List<Room>? Room { get; set; }
+    }
+}
